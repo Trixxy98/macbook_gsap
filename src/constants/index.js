4ff33b47743d7b8b8eaa41131e643f5c5f4b84cd
Trixxy98 <harithfakrullah@gmail.com>
2025-11-20 20:52:09 +0800

    first commit

---
+++ src/constants/index.js
@@ -0,0 +1,9 @@
+const navLinks = [
+    {label: 'iPhone', href: '#'},
+    {label: 'Mac', href: '#'},
+    {label: 'iPad', href: '#'},
+    {label: 'Watch', href: '#'},
+    {label: 'AirPods', href: '#'},
+]
+
+export default navLinks
